263b9214b208cc87aa96becfb37615d8cae5e375
Swairik <swairikdey1001@gmail.com>
2022-07-05 23:45:50 +0530

    update done

--- getLyrics.py
+++ getLyrics.py
@@ -1,26 +1,14 @@
-# import requests
-
-# url = "https://shazam.p.rapidapi.com/auto-complete"
-
-# querystring = {"term":"kiss the","locale":"en-US"}
-
-# headers = {
-#     'x-rapidapi-host': "shazam.p.rapidapi.com",
-#     'x-rapidapi-key': "SIGN-UP-FOR-KEY"
-#     }
-
-# response = requests.request("GET", url, headers=headers, params=querystring)
-
-# print(response.text)
-
 import urllib.request
 import re
 from time import sleep
 import random
 
 def main():
+    artistName = input('Enter the name of the artist : ')
+
     originalLyrics = open('lyrics.txt', 'w')
-    url = "https://www.azlyrics.com/c/coldplay.html"
+
+    url = f'https://www.azlyrics.com/c/{artistName}.html'
     artistHtml = urllib.request.urlopen(url)
     artistHtmlStr = str(artistHtml.read())
 
@@ -30,22 +18,24 @@ def main():
 
     songLinks = []
 
-    for x in links:
-        if "lyrics/coldplay" in x:
-            x = x.replace("..", "")
-            x = "https://www.azlyrics.com/" + x
-            songLinks.append(x)
+    for link in links:
+        if f'lyrics/{artistName}' in link:
+            link = link.replace("..", "")
+            link = "https://www.azlyrics.com/" + link
+            songLinks.append(link)
 
     # print(songLinks)
 
-    for x in songLinks:
-        songHtml = urllib.request.urlopen(x)
+    for songLink in songLinks:
+        songHtml = urllib.request.urlopen(songLink)
         songHtmlStr = str(songHtml.read()) 
+
         #Split the HTML string twice so that we only get the lyrics
         split = songHtmlStr.split('content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->',1)
         split_html = split[1]
-        split = split_html.split('</div>',1)
+        split = split_html.split('</div>', 1)
         lyrics = split[0]
+        
         #Replace the HTML of the lyrics we don't want
         lyrics = lyrics.replace('<br>', '\n')
         lyrics = lyrics.replace('\\', '')
@@ -54,9 +44,12 @@ def main():
         lyrics = lyrics.replace('</i>', '')
         lyrics = lyrics.replace('[Chorus]', '')
         lyrics = lyrics.replace('[Chorus:]', '')
+
         originalLyrics.write(lyrics)
-        sleep (random.randint(2, 10))
-    originalLyrics.close() #We close the file because we no longer have to write anything to it
+        
+        sleep(random.randint(2, 10))
+
+    originalLyrics.close() # We close the file because we no longer have to write anything to it
 
 
 if __name__ == "__main__":
--- markov.py
+++ markov.py
@@ -1,13 +1,4 @@
-'''
-Links:
-1) https://dev.to/catmcgeecode/get-your-first-dev-job-by-building-these-projects-2-markov-chain-lyrics-generator-1kib
-2) https://realpython.com/lyricize-a-flask-app-to-create-lyrics-using-markov-chains/
-3) https://bookdown.org/probability/beta/markov-chains.html (use this to study markov chains)
-'''
-
-# from random import choice
 import random
-import sys
 
 import getLyrics
 
@@ -16,60 +7,50 @@ ErrorsFound = 0
 def generateModel(text, order):
     model = {}
     for i in range(0, len(text) - order):
-        fragment = text[i : i + order]
-        nextLetter = text[i + order]
+        fragment = ''.join(text[i : i + order])
+        nextWord = text[i + order]
         if fragment not in model:
             model[fragment] = {}
-        if nextLetter not in model[fragment]:
-            model[fragment][nextLetter] = 1
+        if nextWord not in model[fragment]:
+            model[fragment][nextWord] = 1
         else:
-            model[fragment][nextLetter] += 1
+            model[fragment][nextWord] += 1
     # print("model : ", model)
+    # print(model)
     return model
 
 def getNextCharacter(model, fragment):
-    letters = []
+    words = []
     try:
-        for letter in model[fragment].keys():
-            for times in range(0, model[fragment][letter]):
-                letters.append(letter)
-        return random.choice(letters)
+        for word in model[fragment].keys():
+            for times in range(0, model[fragment][word]):
+                words.append(word)
+        return random.choice(words)
     except KeyError:
-        print("fragment : ", fragment)
+        # print("fragment : ", fragment)
         global ErrorsFound 
         ErrorsFound += 1
-    # print(letters)
+    # print(words)
     return chr(random.randrange(26) + ord('a'))         # safety net // shouldn't reach here if sample size is big enough
 
 def generateText(text, order, length):
     model = generateModel(text, order)
     currentFragment = text[0 : order]
-    output = ""
+    output = []
     for i in range (0, length - order):
-        newCharacter = getNextCharacter(model, currentFragment)
-        output += newCharacter
-        currentFragment = currentFragment[1 : ] + newCharacter
+        newWord = getNextCharacter(model, ''.join(currentFragment))
+        output.append(newWord)
+        currentFragment.append(newWord)
+        currentFragment = currentFragment[1:]
     if(ErrorsFound) :
         print("Errors Found : ", ErrorsFound)
-    print(output)
-
-# text = "some sample text"     # need more sample size
+    print(' '.join(output))
 
 text = open('lyrics.txt', 'r').read()
 
-# print(text)
-
-# text = "Almost heaven, West Virginia Blue Ridge Mountains, Shenandoah River Life is old there, older than the trees Younger than the mountains, growin' like a breeze Country roads, take me home To the place I belong West Virginia, mountain mama Take me home, country roads All my memories gather 'round her Miner's lady, stranger to blue water Dark and dusty, painted on the sky Misty taste of moonshine, teardrop in my eye Country roads, take me home To the place I belong West Virginia, mountain mama Take me home, country roads I hear her voice in the mornin' hour, she calls me The radio reminds me of my home far away Drivin' down the road, I get a feelin' That I should've been home yesterday, yesterday Country roads, take me home To the place I belong West Virginia, mountain mama Take me home, country roads Country roads, take me home To the place I belong West Virginia, mountain mama Take me home, country roads Take me home, (down) country roads Take me home, (down) country roads"
-
-# text = "So I look in your direction\r\nBut you pay me no attention, do you\r\nI know you don't listen to me\r\n'Cause you say you see straight through me, don't you"
-
-
-# Letters = getNextCharacter(generateModel(text, 2), "so")
-# print(Letters)
-
+text = text.split()
 
 if __name__ == "__main__":
-    # generateText(text, int(sys.argv[1]), int(sys.argv[2]))
-    # pass
     getLyrics.main()
-    generateText(text, 10, 400)
+
+    generateText(text, 1, 400)
